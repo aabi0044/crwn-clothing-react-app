458aa059346a3ce2f21717f4f7fab60fb9d88e53
aabi0044 <mabdulah596@hotmail.com>
2019-12-05 23:55:00 +0500

    routing added to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import  HomePage from './pages/homepage/homepage.component';
-import './App.css';
+import './App.css'; 
+import {Switch, Route} from 'react-router-dom';
+
+
+const HAtsPAge= ()=>(
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div >
-<HomePage/>
+
+      <Switch>
+               <Route exact path='/' component={HomePage} />
+      <Route   path='/hats' component={HAtsPAge} />
+      </Switch>
+ 
+{/* <HomePage/> */}
     </div>
   );
 }
