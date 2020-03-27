bfc922148df7b26aa75eb2ea99a8753dd7e3fec7
aabi0044 <Mabdulah596@hotmail.com>
2020-03-28 00:13:56 +0500

    add qunatity and remove quantity from checkout item component implemented

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,13 +1,30 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartitem = cartItems.find(
-cartItem => cartItem.id === cartItemToAdd.id
+        cartItem => cartItem.id === cartItemToAdd.id
     );
-    if(existingCartitem){
-        return cartItems.map(cartItem=>
-            cartItem.id === cartItemToAdd.id 
-            ? {...cartItem,quantity:cartItem.quantity +1}
-            : cartItem
+    if (existingCartitem) {
+        return cartItems.map(cartItem =>
+            cartItem.id === cartItemToAdd.id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
+        )
+    }
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+}
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+console.log("cart",cartItems);
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    )
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id === cartItemToRemove.id)
+    } else {
+        cartItems.map(cartItem => 
+            cartItem.id === cartItemToRemove.id
+            ? {...cartItem,quantity:cartItem.quantity -1 }
+            :cartItem
             )
     }
-    return [...cartItems,{...cartItemToAdd,quantity:1}]
+
 }
