03f9dc0df689d1521a853c7b6c006f446829f1cf
aabi0044 <Mabdulah596@hotmail.com>
2020-06-02 22:22:03 +0500

    stripe backend node implemented

--- client/src/components/preview-collection/preview-collection.component.jsx
+++ client/src/components/preview-collection/preview-collection.component.jsx
@@ -6,9 +6,7 @@ const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
-
-            {items
-                 
+            {items 
                 .map((item) => (
                     <CollectionItem key={item.id} item={item} />
                       
--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
 const StripeCheckoutButton = ({price})=>{
     const priceForStripe = price * 100;
     const publishablekey = 'pk_test_q1PSZF5er1F152fCUYEdWZYQ00MDL2b23r';
 const onToken = token =>{
+    axios({
+        url:'payment',
+        method: 'POST',
+        data:{
+            amount:priceForStripe,
+            token
+        }
+    }).then(res=>{
+        alert('payment successfull')
+    }).catch(error=>{
+        console.log("payment error",JSON.parse(error));
+        alert("There was an issue with your payments !")
+    })
     console.log(token);
     alert('payment Successful')
 }
